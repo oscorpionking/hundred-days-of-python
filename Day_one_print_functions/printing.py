7c3cdadce36ad3486e00ccc304cacbd50aca0d9a
oscorpionking <ozoemenaisaac5@gmail.com>
2024-02-29 12:44:17 +0100

    Day 1 project completed

---
+++ Day_one_print_functions/printing.py
@@ -0,0 +1,3 @@
+#print functions are used to output text in python. It is enclosed in double or single quotes
+print("welcome to python")
+# the above prints out "Welcome to python", a single line statement
